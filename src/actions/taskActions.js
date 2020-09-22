5d5a48a683c530ab9923da0e6b906a90a8f09755
Andrea <aganesh0988@gmail.com>
2020-09-22 16:12:55 -0500

    defined action creator functions

--- src/actions/taskActions.js
+++ src/actions/taskActions.js
@@ -1,6 +1,17 @@
 // TODO: Define `CREATE_TASK` constant
 // TODO: Define `DELETE_TASK` constant
+export const CREATE_TASK = 'CREATE_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
 
 // TODO: Define `createTask` action creator function
+export const createTask = (taskMessage) => ({
+  type: CREATE_TASK,
+  taskMessage: taskMessage,
+  taskId: new Date().getTime()
+})
 
 // TODO: Define `deleteTask` action creator function
+export const deleteTask = (taskId) => ({
+  type: DELETE_TASK,
+  taskId
+})
